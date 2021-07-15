d805b763eee3de3cf9033161c6deb219e52b8123
ProWON <pro_won45@naver.com>
2021-07-15 19:42:16 +0900

    Upload Project

---
+++ JusolinkExample/Controllers/JusolinkController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Jusolink;
+
+namespace JusolinkExample.Controllers
+{
+    public class JusolinkController : Controller
+    {
+        private readonly JusolinkService _jusolinkService;
+
+        public JusolinkController(JusoInstance JusoInstance)
+        {
+            // 주소링크서비스 객체 생성
+            _jusolinkService = JusoInstance.jusolinkService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View("Search");
+        }
+
+        [HttpGet()]
+        public IActionResult Zipcode(string IndexWord, int? PageNum)
+        {
+            
+            if (IndexWord != null && IndexWord != "")
+            {
+
+                int? perPage = 20;
+                bool noSuggest = false;
+                bool noDiff = false;
+                try
+                {
+                    SearchResult result = _jusolinkService.search(IndexWord, PageNum, perPage, noSuggest, noDiff);
+
+                    return View("Zipcode", result);
+                }
+                catch (JusolinkException pe)
+                {
+                    return View("Exception", pe);
+                }
+            }
+
+            return View("Zipcode");
+        }
+
+        /*
+         * 주소API 이용 단가를 확인합니다.
+         */
+        public IActionResult GetUnitCost()
+        {
+            try
+            {
+                var result = _jusolinkService.GetUnitCost();
+                return View("Result", result);
+            }
+            catch (JusolinkException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 연동회원 잔여포인트를 확인합니다.
+         */
+        public IActionResult GetBalance()
+        {
+            try
+            {
+                var result = _jusolinkService.GetBalance();
+                return View("Result", result);
+            }
+            catch (JusolinkException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+    }
+}
---
+++ JusolinkExample/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Jusolink;
+
+public class JusoInstance
+{
+    //파트너 신청 후 메일로 발급받은 링크아이디(LinkID)와 비밀키(SecretKey)값 으로 변경하시기 바랍니다.
+    private string linkID = "TESTER_JUSO";
+    private string secretKey = "FjaRgAfVUPvSDHTrdd/uw/dt/Cdo3GgSFKyE1+NQ+bc=";
+
+    public JusolinkService jusolinkService;
+
+    public JusoInstance()
+    {
+        //주소링크 서비스 객체 초기화
+        jusolinkService = new JusolinkService(linkID, secretKey);
+    }
+}
+
+namespace JusolinkExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //주소링크 서비스 객체 의존성 주입
+            services.AddSingleton<JusoInstance>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Jusolink}/{action=Index}");
+            });
+        }
+    }
+}
